66d5e5c71b3f2eab717195ebdab2d6021706681c
Thisura1997 <novasoft@gmail.com>
2021-11-08 06:37:58 +0530

    c

--- backend/models/factorial.js
+++ backend/models/factorial.js
@@ -1,10 +1,20 @@
+//sample application
+//required schema and const are declared
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//new schema decleration to get input and output
+
 const FactorialSchema = new Schema({
+//getting the entered values
+
     input: Number,
     result: Number
 });
 
+//final definition
+//passing of values captured from user (keyboard input)
 const FactorialModel = mongoose.model('Factorial', FactorialSchema);
 module.exports = FactorialModel;
